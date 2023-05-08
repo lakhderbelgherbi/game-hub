bf60bbeb847eb60c40ee59d50275f7d01b94578f
Lakhder BELGHERBI <lakhder.belgherbi@outlook.com>
2023-05-08 15:50:23 +0100

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,23 +1,28 @@
-
-import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames from '../hooks/useGames';
-import GameCard from './GameCard';
-
-
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-    const {games, error} = useGames();
-
+  const { games, error, isLoading } = useGames();
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
-        {error && <Text>{error}</Text>}
-        <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} padding='10px' spacing={10}>
-            {games.map(game => 
-            <GameCard game={game} key={game.id} />)}
-        </SimpleGrid>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {isLoading &&
+          skeleton.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+        {games.map((game) => (
+          <GameCard game={game} key={game.id} />
+        ))}
+      </SimpleGrid>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
