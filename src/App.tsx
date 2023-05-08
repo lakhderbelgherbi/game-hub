bfc6150b33a7e2e156e5f7af728253bf91c94316
Lakhder BELGHERBI <lakhder.belgherbi@outlook.com>
2023-05-08 09:54:50 +0100

    Build color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,11 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="Gold">
+        <GridItem area="aside">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         Main
       </GridItem>
     </Grid>
