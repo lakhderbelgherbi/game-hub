d3b24b608c51e8fea9bc7cbf60c908d8a99f5e7a
Lakhder BELGHERBI <lakhder.belgherbi@outlook.com>
2023-05-10 10:38:52 +0100

    Refactor gameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,9 +15,8 @@ const GameGrid = ({gameQuery} : Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  if(error) return <Text>{error}</Text>
   return (
-    <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
@@ -35,7 +34,6 @@ const GameGrid = ({gameQuery} : Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
 
