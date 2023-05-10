7f21e2940113b5e09e07819b0d3bad672960a018
Lakhder BELGHERBI <lakhder.belgherbi@outlook.com>
2023-05-10 10:12:15 +0100

    Add Imojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import { Game } from "../hooks/useGames"
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
 import noImage from '../assets/no-image-placeholder-6f3882e0.webp'
+import Emoji from './Emoji'
 
 
 interface Props {
@@ -19,7 +20,7 @@ const GameCard = ({game }: Props) => {
           <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>{game.name}</Heading>
+        <Heading fontSize='2xl'>{game.name} <Emoji rating={game.rating_top} /></Heading>
       </CardBody>
     </Card>
   )
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,6 +8,8 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
+  rating: number
 }
 
 const useGames = (gameQuery: GameQuery) =>
