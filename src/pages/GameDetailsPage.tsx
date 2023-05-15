e3e22aec932c217c95e007b6ee23d04bfc025ec5
Lakhder BELGHERBI <lakhder.belgherbi@outlook.com>
2023-05-15 10:10:12 +0100

    Improving layou(take to columns) and fix the navbar

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,27 +1,31 @@
-import { Heading, Spinner } from '@chakra-ui/react';
-import { useParams } from 'react-router-dom';
-import ExpandableText from '../components/ExpandableText';
-import GameAttributes from '../components/GameAttributes';
-import GameScreenshot from '../components/GameScreenshot';
-import GameTrailer from '../components/GameTrailer';
-import useGame from '../hooks/useGame';
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
+import GameScreenshot from "../components/GameScreenshot";
+import GameTrailer from "../components/GameTrailer";
+import useGame from "../hooks/useGame";
 
 const GameDetailsPage = () => {
-    const { slug } = useParams();
-    const {data: game, error, isLoading} = useGame(slug!);
+  const { slug } = useParams();
+  const { data: game, error, isLoading } = useGame(slug!);
 
-    if (isLoading) return <Spinner />
-    if (error || !game) throw error;
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
 
   return (
-    <>
-        <Heading>{ game.name }</Heading>
-        <ExpandableText>{ game.description_raw }</ExpandableText>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
         <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
         <GameTrailer gameId={game.id} />
         <GameScreenshot gameId={game.id} />
-    </>
-  )
-}
+      </GridItem>
+    </SimpleGrid>
+  );
+};
 
-export default GameDetailsPage
+export default GameDetailsPage;
